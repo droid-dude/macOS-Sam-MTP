b2def25917bd95b41d50d23b6473b691dd68a553
akadriu <akadriu@worcester.edu>
2024-12-02 17:45:45 -0500

    Initial Commit

---
+++ Samsung FRP/Samsung_FRPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Samsung_FRPApp.swift
+//  Samsung FRP
+//
+//  Created by Arber Kadriu on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Samsung_FRPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Samsung FRPTests/Samsung_FRPTests.swift
@@ -0,0 +1,17 @@
+//
+//  Samsung_FRPTests.swift
+//  Samsung FRPTests
+//
+//  Created by Arber Kadriu on 12/2/24.
+//
+
+import Testing
+@testable import Samsung_FRP
+
+struct Samsung_FRPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
